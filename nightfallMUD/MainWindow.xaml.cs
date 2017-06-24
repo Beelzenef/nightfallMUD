f6931e770de745bd852797a7086e1f36a8452d68
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2017-06-24 15:58:17 +0200

    inicio + gui

---
+++ nightfallMUD/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace nightfallMUD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtB_ComandosUsuario.Text.Length != 0)
+            {
+                lsB_Display.Items.Add(txtB_ComandosUsuario.Text);
+                txtB_ComandosUsuario.Clear();
+                txtB_ComandosUsuario.Focus();
+            }
+        }
+    }
+}
